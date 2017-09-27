c21649659ff9c10810959903197d318b50a684ab
lalexander2 <lalexander2@loyola.edu>
2017-09-27 14:30:50 -0400

    HW3 is ez

---
+++ Raenita.py
@@ -0,0 +1,11 @@
+name = input("What is your name?")
+if name == "Raenita":
+    age = int(input("How old are you?"))
+    if age >= 50:
+        print("I see you, 30 years from now, living a happy life with your children and grandchildren. You will live to be 107 years old.")
+    elif age >= 30:
+        print("Though your journey through life so far has been great, there is much more to come. Your full potential has not yet been realized.")
+    else:
+        print("Your life has not yet begun. You are going to invent something that will change the world for many people.")
+else:
+    print("Your fortune is dark. You will lose all that you are carrying when you next leave your house.")
